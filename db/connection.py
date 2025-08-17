ad13568d2edf46caf152aa96b8df1e2f81f12888
iojan715 <rolax.mejor@gmail.com>
2025-08-16 22:36:21 -0700

    Add files via upload

--- db/connection.py
+++ db/connection.py
@@ -1,14 +1,22 @@
 import asyncpg
+import ssl
 from config import DATABASE_URL
 
 # Pool global
 db_pool = None
 
-# Crear el pool (una vez)
 async def create_pool():
     global db_pool
-    db_pool = await asyncpg.create_pool(DATABASE_URL)
+    
+    # Detectar si es enlace externo o interno
+    # (generalmente los internos no llevan ".render.com", ".supabase.co", etc.)
+    use_ssl = any(x in DATABASE_URL for x in ["render.com", "supabase.co", "neon.tech", "railway.app"])
+    
+    ssl_ctx = None
+    if use_ssl:
+        ssl_ctx = ssl.create_default_context()
+    
+    db_pool = await asyncpg.create_pool(DATABASE_URL, ssl=ssl_ctx)
 
-# Obtener el pool desde otros archivos
 def get_pool():
     return db_pool
