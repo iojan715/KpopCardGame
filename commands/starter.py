1cbf65f5f07e8c9168b60aa4195629eda36cb4d5
iojan715 <rolax.mejor@gmail.com>
2025-09-20 20:39:25 -0700

    Add files via upload

--- commands/starter.py
+++ commands/starter.py
@@ -7,7 +7,7 @@ from utils.localization import get_translation
 from utils.language import get_user_language
 from asyncpg import Pool
 
-version = "?v=242"
+version = "?v=241"
 base = 200
 mult = 600
 reduct = 100000
@@ -422,5 +422,4 @@ class SponsorCommand(commands.Cog):
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(StartCommand(bot))
-
     await bot.add_cog(SponsorCommand(bot))
